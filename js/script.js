12513ed62a9e658f5f915e563fb18776dfa2f217
alvarogabriel <alvarogabriel1103@hotmail.com>
2024-05-26 11:45:38 -0300

    upload

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ const input__send = send__messagem.querySelector('input');
 const username = document.querySelector('.username');
 const div__messagem = document.querySelector(".div__messagem");
 
-let ws = new WebSocket('https://star-notch-barberry.glitch.me');
+const ws = new WebSocket('https://star-notch-barberry.glitch.me');
+
+const ids = [];
 
 const colors = [
     "blue",
@@ -17,17 +19,34 @@ const colors = [
     "pink",
     "chocolate",
     "orange",
-    "yellow",
+    "darkgoldenrod",
     "black",
     "purple"
 ];
 
 ws.addEventListener('message',function(event) {
-    const 
-    const range = document.createRange();
-    const fragment = range.createContextualFragment(event.data);
+    const message = JSON.parse(event.data);
+    sessionStorage.id = message.id;
+    const div = document.createElement("div");
+    div.setAttribute('data-user-id', message.id);
 
-    div__messagem.appendChild(fragment);
+    const username = document.createElement('span');
+    username.classList.add('username');
+    username.style.color = message.color;
+    username.textContent = message.username;
+
+    const content = document.createElement('span');
+    content.textContent = message.content;
+
+    div.appendChild(username);
+    div.appendChild(content);
+    
+    if (message.id === user.id) {
+        div.classList.add('right-message');
+    }
+    else{div.classList.add('left-message')};
+
+    div__messagem.appendChild(div)
 })
 
 const user = {
@@ -51,9 +70,11 @@ create__username.addEventListener('submit',event => {
     user.color = RandomColor(colors);
     user.id = `id-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
 
+    div__messagem.style.display = 'block';
+
     localStorage.id = user.id;
     container.style.width= '800px';
-    container.style.height = '100%';
+    container.style.height = '100vh';
     container.style.borderRadius = '0';
 });
 
@@ -69,25 +90,24 @@ function CreateElement (input) {
     const message = document.createElement('span');
     message.textContent = input.value;
 
-    const id = document.createElement('span');
-    id.classList.add('id');
-    id.textContent = user.id;
-
     div.appendChild(username);
     div.appendChild(message);
-    div.appendChild(id);
-
+    div.setAttribute('data-user-id',user.id);
     return div.outerHTML
 }
 
 send__messagem.addEventListener('submit',event => {
     event.preventDefault();
     if (ws.readyState === WebSocket.OPEN) {
-        div__messagem.style.display = 'block';
-        ws.send(CreateElement(input__send));
+        
+        const message = {
+            id : user.id,
+            username: user.username,
+            color: user.color,
+            content: input__send.value
+        }
+        ws.send(JSON.stringify(message));
         input__send.value = '';
     }
 
 });
-
-setTimeout(() => console.log(user),5000);
